4ef3d8e5ea790f518b7ace330e02184caa298f2c
Michael Thelen <michael.thelen@gmail.com>
2017-03-09 15:10:19 -0800

    Add server file

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/youawake', function(req, res){
+  res.send("Hello there!");
+})
+
+
+app.listen(3000, function(){
+  console.log("listening on 3000");
+})
